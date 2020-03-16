b820fadc561b22b3afc505bce9ca7bcc9d0627db
RayRay123-99 <hartonoraymond789@gmail.com>
2020-03-16 13:40:19 +0800

    This is the front end

--- Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/FragmentDiscovery.java
+++ Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/FragmentDiscovery.java
@@ -1,6 +1,6 @@
 package com.IrisBICS.lonelysg;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +8,66 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-public class FragmentDiscovery extends Fragment {
+public class FragmentDiscovery extends Fragment implements View.OnClickListener {
+    private CardView foodIcon, movieIcon, othersIcon, sportsIcon, studyIcon;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discovery, container, false);
+        View v = inflater.inflate(R.layout.fragment_discovery, container, false);
+
+        // Defining cards
+        foodIcon = (CardView) v.findViewById(R.id.foodIcon);
+        movieIcon = (CardView) v.findViewById(R.id.movieIcon);
+        othersIcon = (CardView) v.findViewById(R.id.othersIcon);
+        sportsIcon = (CardView) v.findViewById(R.id.sportsIcon);
+        studyIcon = (CardView) v.findViewById(R.id.studyIcon);
+
+        // Add Click listener to the cards
+        foodIcon.setOnClickListener(this);
+        movieIcon.setOnClickListener(this);
+        othersIcon.setOnClickListener(this);
+        sportsIcon.setOnClickListener(this);
+        studyIcon.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.foodIcon :
+                i = new Intent(this.getActivity(), InvitationsUI.class);
+                startActivity(i);
+                break;
+
+            case R.id.movieIcon :
+                i = new Intent(this.getActivity(), InvitationsUI.class);
+                startActivity(i);
+                break;
+
+            case R.id.othersIcon :
+                i = new Intent(this.getActivity(), InvitationsUI.class);
+                startActivity(i);
+                break;
+
+            case R.id.sportsIcon :
+                i = new Intent(this.getActivity(), InvitationsUI.class);
+                startActivity(i);
+                break;
+
+            case R.id.studyIcon :
+                i = new Intent(this.getActivity(), InvitationsUI.class);
+                startActivity(i);
+                break;
+
+            default :
+                break;
+        }
     }
 }
--- Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/FragmentInvitation.java
+++ Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/FragmentInvitation.java
@@ -1,20 +1,107 @@
 package com.IrisBICS.lonelysg;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.IrisBICS.lonelysg.R;
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class FragmentInvitation extends Fragment {
+    // For dropdown box
+    Spinner dropdownbox;
+    String categories[] = {"Choose your invitation category", "Food and Drinks", "Movies", "Sports", "Study", "Others"};
+    ArrayAdapter<String>arrayAdapter;
+
+    //For time and date selection
+    Button datePick, timePick;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_invitation, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.fragment_invitation, container, false);
+
+        // For dropdown box (category selection)
+        dropdownbox = (Spinner) v.findViewById(R.id.categoryDropBox);
+        arrayAdapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_list_item_1, categories);
+        dropdownbox.setAdapter(arrayAdapter);
+
+        datePick = v.findViewById(R.id.datePick);
+        timePick = v.findViewById(R.id.timePick);
+
+        // For date selection
+        datePick.setOnClickListener(new View.OnClickListener() {
+            @Nullable
+            @Override
+            public void onClick (View view){
+                Calendar calender = Calendar.getInstance();
+                // Current date shown when button is clicked
+                int YEAR = calender.get(Calendar.YEAR);
+                int MONTH = calender.get(Calendar.MONTH); // Month 0 is January
+                int DATE = calender.get(Calendar.DATE);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(v.getContext()  , new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        String dateString = year + " " + month + " " + date;
+                        datePick.setText(dateString);
+
+                        // For date formatting
+                        /*
+                        Calendar calendar1 = Calendar.getInstance();
+                        calendar1.set(Calendar.YEAR, year);
+                        calendar1.set(Calendar.MONTH, month);
+                        calendar1.set(Calendar.DATE, date);
+
+                        CharSequence dateCharSequence = DateFormat.format("EEEE, dd MMM yyyy", calendar1);
+                        datePick.setText(dateCharSequence);
+                         */
+                    }
+                }, YEAR, MONTH, DATE);
+
+                datePickerDialog.show();
+            }
+        });
+
+        // For time selection
+        timePick.setOnClickListener(new View.OnClickListener() {
+            @Nullable
+            @Override
+            public void onClick (View view) {
+                Calendar calender = Calendar.getInstance();
+                // Current time shown when button is clicked
+                int HOUR = calender.get(Calendar.HOUR);
+                int MINUTE = calender.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        String timeString = "hour: " + hour + "minute: " + minute;
+                        datePick.setText(timeString);
+                    }
+                }, HOUR, MINUTE, true);
+
+                timePickerDialog.show();
+            }
+        });
+
+        return v;
     }
+
 }
