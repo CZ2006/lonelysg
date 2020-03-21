56295974a35c43449d7cfc625206466f0c111e08
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-03-21 19:47:22 +0800

    Update and delete functions

--- backend/functions/routes/Iris/api.js
+++ backend/functions/routes/Iris/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/getStuff", (req,res)=>{ //Sample function fetching data from the realtime DB on firebase
+router.get("/getUsers", (req,res)=>{ //Sample function fetching data from the realtime DB on firebase
     //Call using http://localhost:5001/lonely-4a186/us-central1/app/Iris/getStuff after running firebase serve
     let database = req.app.get("database")
     var ref = database.ref('User');
@@ -11,15 +11,14 @@ router.get("/getStuff", (req,res)=>{ //Sample function fetching data from the re
     })
 })
 
-router.post("/addUser", (req, res) => { //Sample function adding new user to realtime DB on firebase
-    //Call using http://localhost:5001/lonely-4a186/us-central1/app/Iris/addUser in postman with sample data in the body after running firebase serve
+router.post("/addUser", (req, res) => { 
     let database = req.app.get("database")
     let userRef = database.ref("User")
 
     const newUser = userRef.child("User" + req.body.userID) //Requires information sent in JSON format and containing at least userID
     newUser.set(req.body)
 
-    res.end("Posted!"); //Returned in postman
+    res.end("User" + req.body.userID + " added inside the DB!"); //Returned in postman
 
     /* Sample data to test this function:
 
@@ -37,7 +36,61 @@ router.post("/addUser", (req, res) => { //Sample function adding new user to rea
     } */
 })
 
-//Update user
-//Delete user
+router.delete("/deleteUser", (req, res) => {
+
+    let database = req.app.get("database")
+    let userToDel = database.ref("User/User" + req.body)
+
+    userToDel.set({})
+
+    res.end(req.body + " deleted from the DB!")
+
+})
+
+
+router.post("/updateUser", (req, res) => {
+
+    let database = req.app.get("database")
+    let userToUpdate = database.ref("User/User" + req.body.userID)
+
+    async function updateUser() {
+        var newData;
+
+        await userToUpdate.once("value", function(snapshot){
+            newData = snapshot.val()
+        })
+
+        for (key in req.body) {
+            newData[key] = req.body[key]
+        }
+    
+        userToUpdate.set(newData)
+    
+        res.end("User" + req.body.userID + " updated!")
+    }
+
+    updateUser()
+
+    /* Sample data to test this function:
+
+    {
+        "userID": 2,
+        "password": "newPa55w0rd...",
+        "interests": "Youtube, Marvel"
+    } */
+
+})
 
 module.exports = router;
+
+//Todo
+
+// - getUser
+// - addUser
+// - removeUser
+// - editUser
+
+// - addInvitation
+// - removeInvitation
+// - getInvitation
+// - editInvitation
