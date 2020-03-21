63b19805c15fb13a8bb2b404498f69b5b5272e36
winhuiwy <winhuiwy@users.noreply.github.com>
2020-03-22 00:04:18 +0800

    Added logging out function

--- Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/LoginPage.java
+++ Lonelysg/app/src/main/java/com/IrisBICS/lonelysg/LoginPage.java
@@ -1,21 +1,11 @@
 package com.IrisBICS.lonelysg;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.SpannableStringBuilder;
-import android.text.style.ImageSpan;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class LoginPage extends AppCompatActivity {
     MeowBottomNavigation meo;
@@ -64,5 +54,10 @@ public class LoginPage extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, select_fragment).commit();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 }
